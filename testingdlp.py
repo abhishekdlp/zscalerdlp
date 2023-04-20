cc83a6179a12f8426539a0c9b92f7218372d8280
abhishekdlp <131152416+abhishekdlp@users.noreply.github.com>
2023-04-20 11:44:04 +0530

    Create testingdlp.py

---
+++ testingdlp.py
@@ -0,0 +1,36 @@
+this is testing my DLP functions, here are the sample creditcards
+
+Credit Card number 1 : 4929802767246662
+Credit Card number 2 : 4716452070411681
+Credit Card number 3 : 4556332529164326
+Credit Card number 4 : 4556917143015787
+Credit Card number 5 : 4024007163686750
+
+Credit Card number 6 : 5245279129889865
+
+Credit Card number 7 : 5247143283341915
+
+Credit Card number 8 : 5113607352639616
+Credit Card number 9 : 5467264812100052
+
+Credit Card number 10 : 5272997598666656
+
+Credit Card number 11 : 379692826671817
+Credit Card number 12 : 342067180876167
+  
+  
+  SSN	gender	birthdate	maiden name	last name	first name	address	city	state	zip	phone	email	cc_type	CCN	cc_cvc	cc_expiredate
+172-32-1176	m	4/21/1958	Smith	White	Johnson	10932 Bigge Rd	Menlo Park	CA	94025	408 496-7223	jwhite@domain.com	m	5270-4267-6450-5516	123	6/25/2010
+514-14-8905	f	12/22/1944	Amaker	Borden	Ashley	4469 Sherman Street	Goff	KS	66428	785-939-6046	aborden@domain.com	m	5370-4638-8881-3020	713	2/1/2011
+213-46-8915	f	4/21/1958	Pinson	Green	Marjorie	309 63rd St. #411	Oakland	CA	94618	415 986-7020	mgreen@domain.com	v	4916-9766-5240-6147	258	2/25/2009
+524-02-7657	m	3/25/1962	Hall	Munsch	Jerome	2183 Roy Alley	Centennial	CO	80112	303-901-6123	jmunsch@domain.com	m	5180-3807-3679-8221	612	3/1/2010
+489-36-8350	m	9/6/1964	Porter	Aragon	Robert	3181 White Oak Drive	Kansas City	MO	66215	816-645-6936	raragon@domain.com	v	4929-3813-3266-4295	911	12/1/2011
+514-30-2668	f	5/27/1986	Nicholson	Russell	Jacki	3097 Better Street	Kansas City	MO	66215	913-227-6106	jrussell@domain.com	a	3.4539E+14	232	1/1/2010
+505-88-5714	f	9/23/1963	Mcclain	Venson	Lillian	539 Kyle Street	Wood River	NE	68883	308-583-8759	lvenson@domain.com	d	3.02049E+13	471	12/1/2011
+690-05-5315	m	10/2/1969	Kings	Conley	Thomas	570 Nancy Street	Morrisville	NC	27560	919-656-6779	tconley@domain.com	v	4916 4811 5814 8111	731	10/1/2010
+646-44-9061	M	1/12/1978	Kurtz	Jackson	Charles	1074 Small Street	New York	NY	10011	212-847-4915	cjackson@domain.com	m	5218 0144 2703 9266	892	11/1/2011
+421-37-1396	f	4/9/1980	Linden	Davis	Susan	4222 Bedford Street	Jasper	AL	35501	205-221-9156	sdavis@domain.com	v	4916 4034 9269 8783	33	4/1/2011
+461-97-5660	f	1/4/1975	Kingdon	Watson	Gail	3414 Gore Street	Houston	TX	77002	713-547-3414	gwatson@domain.com	v	4532 1753 6071 1112	694	9/1/2011
+660-03-8360	f	7/11/1953	Onwunli	Garrison	Lisa	515 Hillside Drive	Lake Charles	LA	70629	337-965-2982	lgarrison@domain.com	v	4539 5385 7425 5825	680	6/1/2011
+751-01-2327	f	2/16/1968	Simpson	Renfro	Julie	4032 Arron Smith Drive	Kaunakakai	HI	96748	808-560-1638	jrenfro@domain.com	m	5325 3256 9519 6624	238	3/1/2009
+559-81-1301	m	1/20/1952	Mcafee	Heard	James	2865 Driftwood Road	San Jose	CA	95129	408-370-0031	jheard@domain.com	v	4532 4220 6922 9909	311	9/1/2010
